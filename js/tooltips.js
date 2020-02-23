8560f9dc113a0359235f82a11c74902a910a93c2
Matheus Henrique <44712248+matheushenriquefs@users.noreply.github.com>
2020-02-23 02:47:58 -0300

    Atualizar tooltips.js

--- js/tooltips.js
+++ js/tooltips.js
@@ -2,10 +2,18 @@
  * Tooltips que ajudam o usuario a preencher os campos do formulario corretamente
  */
 
-$("#inputVeiculo").tooltip({"trigger": "focus", "title": "Um veículo válido NÃO deve conter caracteres especiais"});
+$("#inputAdicionarVeiculo").tooltip({"trigger": "focus", "title": "Um veículo válido NÃO deve conter caracteres especiais"});
 
-$("#inputMarca").tooltip({"trigger": "focus", "title": "Uma marca válida NÃO deve conter caracteres especiais"});
+$("#inputAdicionarMarca").tooltip({"trigger": "focus", "title": "Uma marca válida NÃO deve conter caracteres especiais"});
 
-$("#inputAno").tooltip({"trigger": "focus", "title": "Um ano válido NÃO deve conter letras, caracteres especiais ou números negativos"});
+$("#inputAdicionarAno").tooltip({"trigger": "focus", "title": "Um ano válido NÃO deve conter letras, caracteres especiais ou números negativos"});
 
-$("#inputDescricao").tooltip({"trigger": "focus", "title": "Faça uma breve descrição do veículo"});
+$("#inputAdicionarDescricao").tooltip({"trigger": "focus", "title": "Faça uma breve descrição do veículo"});
+
+$("#inputEditarVeiculo").tooltip({"trigger": "focus", "title": "Um veículo válido NÃO deve conter caracteres especiais"});
+
+$("#inputEditarMarca").tooltip({"trigger": "focus", "title": "Uma marca válida NÃO deve conter caracteres especiais"});
+
+$("#inputEditarAno").tooltip({"trigger": "focus", "title": "Um ano válido NÃO deve conter letras, caracteres especiais ou números negativos"});
+
+$("#inputEditarDescricao").tooltip({"trigger": "focus", "title": "Faça uma breve descrição do veículo"});
