81da33c8fa604c4d4fadd7495361f78a580e306e
Matheus Henrique <44712248+matheushenriquefs@users.noreply.github.com>
2020-02-23 02:28:25 -0300

    Melhorar a legibilidade do codigo

--- js/utils.js
+++ js/utils.js
@@ -2,70 +2,66 @@
  * Limpa os campos do formulario e define o valor padrao para o input radio do form
  * 
  * @param {Array} inputs - Um array com os inputs que deseja limpar
+ * @param {String} action - Uma string com a a primeira letra maiuscula,
+ * serve para indicar qual input vendido a funcao ira alterar
  */
 
-function clearInputs(inputs){
-    for(let val of inputs){
-      $(`#${val}`).removeClass("border-success");
-      $(`#${val}`).removeClass("border-danger");
-      $(`#${val}`).removeClass("bg-white");
-      $(`#${val}`).removeAttr("placeholder");
-      $(`#${val}`).removeAttr("disabled");
-      $(`#${val}`).val("");
-      $(`#${val}Text`).removeClass("text-success");
-      $(`#${val}Text`).removeClass("text-danger");
-      $(`#${val}Text`).text("");
-    }
-    $("#inputVendidoNao").removeAttr("disabled");
-    $("#inputVendidoSim").removeAttr("disabled");
-    $("#inputVendidoNao").prop("checked", true);
+function clearInputs(inputs, action){
+  for(let val of inputs){
+    $("#" + val).removeClass("border-success");
+    $("#" + val).removeClass("border-danger");
+    $("#" + val).val("");
+    $("#" + val + "Text").removeClass("text-success");
+    $("#" + val + "Text").removeClass("text-danger");
+    $("#" + val + "Text").text("");
+  }
+  $("#input" + action + "VendidoNao").prop("checked", true);
 }
 
 /**
  * @param {Array} inputs - um array com os inputs que deseja popular
  * @param {Object} obj - objeto contendo as informacoes necessarias para
  * preencher os campos
- * @param {String} btnAction - uma string com a nome da classe do botao
+ * @param {String} action - Uma string com a a primeira letra maiuscula,
+ * serve para indicar qual input vendido a funcao ira alterar
  */
 
-function populateInputs(inputs, obj, btnAction){
-    let array = [];
-    if(btnAction === "btnModalInfo"){
-      for(let val in obj){
-        if(val !== "id"){
-          array.push(obj[val]);
-        }
-      }
-      for(let i = 0; i <= inputs.length; i++){
-        if(array[i] === "Sim"){
-          $("#inputVendidoSim").removeAttr("disabled");
-          $("#inputVendidoSim").prop("checked", true);
-          $("#inputVendidoNao").attr("disabled", "");
-        }else{
-          $("#inputVendidoNao").removeAttr("disabled");
-          $("#inputVendidoNao").prop("checked", true);
-          $("#inputVendidoSim").attr("disabled", "");
-        }
-        $(`#${inputs[i]}`).attr("placeholder", array[i]);
-        $(`#${inputs[i]}`).attr("disabled", "");
-        $(`#${inputs[i]}`).addClass("bg-white");
-      }
-    }else if(btnAction === "btnModalEditar"){
-      let array = [];
-      for(let val in obj){
-        if(val !== "id" && val !== "update_at"){
-          array.push(obj[val]);
-        }
-      }
-      for(let i = 0; i <= inputs.length; i++){
-        if(array[i] === "Sim"){
-          $("#inputVendidoSim").prop("checked", true);
-        }else{
-          $("#inputVendidoNao").prop("checked", true);
-        }
-        $(`#${inputs[i]}`).val(array[i]);
+function populateInputs(inputs, obj, action){
+  let array = [];
+  if(action === "Info"){
+    for(let val in obj){
+      if(val !== "id"){
+        array.push(obj[val]);
       }
     }
+    for(let i = 0; i <= inputs.length; i++){
+      if(array[i] === "Sim"){
+        $("#input" + action + "VendidoSim").removeAttr("disabled");
+        $("#input" + action + "VendidoSim").prop("checked", true);
+        $("#input" + action + "VendidoNao").attr("disabled", "");
+      }else{
+        $("#input" + action + "VendidoNao").removeAttr("disabled");
+        $("#input" + action + "VendidoNao").prop("checked", true);
+        $("#input" + action + "VendidoSim").attr("disabled", "");
+      }
+      $("#" + inputs[i]).attr("placeholder", array[i]);
+    }
+  }else if(action === "Editar"){
+    let array = [];
+    for(let val in obj){
+      if(val !== "id" && val !== "update_at"){
+        array.push(obj[val]);
+      }
+    }
+    for(let i = 0; i <= inputs.length; i++){
+      if(array[i] === "Sim"){
+        $("#input" + action + "VendidoSim").prop("checked", true);
+      }else{
+        $("#input" + action + "VendidoNao").prop("checked", true);
+      }
+      $("#" + inputs[i]).val(array[i]);
+    }
+  }
 }
 
 /**
